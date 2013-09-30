488e5804bcd51f657c52c0d9a6df27cefefc5e2c
Paul Cortens <paul@thoughtless.ca>
2013-09-30 09:59:41 -0700

    bump version
    update changelog
    update Gemfile to explicitly point to rubygems.org

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+source 'https://rubygems.org'
+
 group :test do
   gem 'rack-test'
   gem 'rspec'
--- lib/async_rack_test/version.rb
+++ lib/async_rack_test/version.rb
@@ -1,5 +1,5 @@
 module AsyncRackTest # :nodoc:
   module Version
-    STRING = '0.0.2'
+    STRING = '0.0.3'
   end
 end
