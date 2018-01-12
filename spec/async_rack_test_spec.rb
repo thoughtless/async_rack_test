659e71669dbd3bb2f9a902c178aa9c1fdd00a964
Paul <paul@thoughtless.ca>
2018-01-12 11:28:06 -0800

    Update gems and spec syntax

--- spec/async_rack_test_spec.rb
+++ spec/async_rack_test_spec.rb
@@ -17,32 +17,32 @@ describe AsyncRackTest do
     let(:test_obj) { DummyTest.new }
     describe '#sync_app' do
       it 'should be a ResyncApp' do
-        test_obj.sync_app.should be_kind_of(AsyncRackTest::ResyncApp)
+        expect(test_obj.sync_app).to be_kind_of(AsyncRackTest::ResyncApp)
       end
     end
 
     describe '#async_app' do
       it 'should be the original app' do
-        test_obj.async_app.should be_kind_of(Proc)
+        expect(test_obj.async_app).to be_kind_of(Proc)
       end
     end
 
     describe '#app' do
       context 'default' do
         it 'should be the original app' do
-          test_obj.app.should == test_obj.async_app
+          expect(test_obj.app).to eql(test_obj.async_app)
         end
       end
       context 'after calling #use_sync' do
         it 'should be a ResyncApp' do
           test_obj.use_sync
-          test_obj.app.should == test_obj.sync_app
+          expect(test_obj.app).to eql(test_obj.sync_app)
         end
         context 'then after calling #use_async' do
           it 'should be the original app' do
             test_obj.use_sync
             test_obj.use_async
-            test_obj.app.should == test_obj.async_app
+            expect(test_obj.app).to eql(test_obj.async_app)
           end
         end
       end
@@ -50,22 +50,22 @@ describe AsyncRackTest do
 
     describe '#aget' do
       it 'should set the app be a ResyncApp' do
-        test_obj.should_receive(:use_sync)
-        test_obj.stub(:get)
-        test_obj.stub(:use_async)
+        expect(test_obj).to receive(:use_sync)
+        allow(test_obj).to receive(:get)
+        allow(test_obj).to receive(:use_async)
         test_obj.aget '/'
       end
       it 'should call #call on #sync_app' do
-        test_obj.sync_app.should_receive(:call).and_return([200, {}, []])
+        expect(test_obj.sync_app).to receive(:call).and_return([200, {}, []])
         test_obj.aget '/'
       end
       it 'should call #get' do
-        test_obj.should_receive(:get)
+        expect(test_obj).to receive(:get)
         test_obj.aget '/'
       end
       it 'should put the app back to the original' do
         test_obj.aget '/'
-        test_obj.app.should == test_obj.async_app
+        expect(test_obj.app).to eql(test_obj.async_app)
       end
     end
   end
@@ -75,7 +75,7 @@ describe AsyncRackTest do
     let(:resync_app) { AsyncRackTest::ResyncApp.new(DummyTest.new.app) }
     describe '#call' do
       it "should call #call on the passed in app" do
-        resync_app.app.should_receive(:call).and_return([200, {}, []])
+        expect(resync_app.app).to receive(:call).and_return([200, {}, []])
         resync_app.call({})
       end
 
@@ -95,10 +95,10 @@ describe AsyncRackTest do
         thread = Thread.new { @result = resync_app.call({}) }
 
         sleep 1 # Give some time to guarantee the thread has had time to run.
-        @result.should be_nil
+        expect(@result).to be_nil
         @trigger_async = true
         thread.join
-        @result.should == [200, {}, []]
+        expect(@result).to eql([200, {}, []])
       end
 
       it "should return when the passed in app throws :async", :slow => true do
@@ -117,10 +117,10 @@ describe AsyncRackTest do
         thread = Thread.new { @result = resync_app.call({}) }
 
         sleep 1 # Give some time to guarantee the thread has had time to run.
-        @result.should be_nil
+        expect(@result).to be_nil
         @trigger_async = true
         thread.join
-        @result.should == [200, {}, []]
+        expect(@result).to eql([200, {}, []])
       end
 
       it "should return instantly when the passed in app is not async", :slow => true do
@@ -139,7 +139,7 @@ describe AsyncRackTest do
         thread = Thread.new { @result = resync_app.call({}) }
 
         sleep 1 # Give some time to guarantee the thread has had time to run.
-        @result.should == [200, {}, []]
+        expect(@result).to eql([200, {}, []])
         @trigger_async = true
         sleep 1 # Give some time to guarantee the thread has had time to run.
         thread.join
@@ -157,9 +157,9 @@ describe AsyncRackTest do
         let(:resync_app) { AsyncRackTest::ResyncApp.new(slow_app, :timeout => 1) }
 
         it "should time out" do
-          lambda {
+          expect {
             resync_app.call({})
-          }.should raise_error(AsyncRackTest::Timeout)
+          }.to raise_error(AsyncRackTest::Timeout)
         end
       end
     end
